8da0e6fd99f3f08bcd553298975c838ce2e6474c
XiaWeilong <1017502203@qq.com>
2023-03-22 13:45:33 +0800

    modify hw10

--- L10/rotation_update/main.cpp
+++ L10/rotation_update/main.cpp
@@ -13,22 +13,19 @@ Eigen::Matrix3d skew_symmetric(const Eigen::Vector3d &w)
 
 int main()
 {
-    Eigen::Vector3d w(0.01, 0.02, 0.03);
-
     Eigen::Vector4d q_random = Eigen::Vector4d::Random();
     q_random.normalize();
-    std::cout << "q_random: " << q_random.transpose() << std::endl;
-    std::cout << "q_random norm: " << q_random.norm() << std::endl;
     Eigen::Quaterniond q_init(q_random[0], q_random[1], q_random[2], q_random[3]);
 
     Eigen::Matrix3d R_init = q_init.toRotationMatrix();
 
+    Eigen::Vector3d w(0.01, 0.02, 0.03);
     Eigen::Quaterniond delta_q(1, 0.5 * w.x(), 0.5 * w.y(), 0.5 * w.z());
     Eigen::Quaterniond q_update = q_init * delta_q;
     q_update.normalize();
     Eigen::Matrix3d R_update = R_init * skew_symmetric(w).exp();
-    Eigen::Quaterniond q_verify(R_update);
 
+    Eigen::Quaterniond q_verify(R_update);
     std::cout << "q_update: " << q_update.w() << " " << q_update.x() << " " << q_update.y() << " " << q_update.z()
               << std::endl;
     std::cout << "q_verify: " << q_verify.w() << " " << q_verify.x() << " " << q_verify.y() << " " << q_verify.z()
